8b273d6c3ee73b83e5bff2ea3e075dc65259d0a6
Nikita <karyakin@surfstudio.ru>
2022-11-20 17:57:58 +0300

    1

--- task_1/solution.js
+++ task_1/solution.js
@@ -14,6 +14,9 @@ function addInCartMessage(title, price) {
     // добавьте в конец переменной message текст 'теперь в корзине!'
     // выведите переменную message в консоль
     
+    let message = productName + " за " + productPrice + " теперь в корзине!";
+    console.log(message);
+
 
     // Конец решения задания №1.1.
 
@@ -28,8 +31,12 @@ function addInCartCountChange(value) {
     // создайте переменную newValue
     // присвойте ей значение oldValue
     // увеличьте значение newValue на 1
+    let newValue = oldValue;
+    newValue ++;
+
 
     // Конец решения задания №1.2.
+
 
     return newValue;
 }
@@ -44,6 +51,9 @@ function addInCartSumChange(sum, delta) {
     // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
     // замените склеивание, используемое в newSum Text на шаблонную строку
 
+    let newSum = oldSum + difference;
+    let newSumText  = newSum + "₽";
+    newSumText = '${newSum} + "₽"';
     // Конец решения задания №1.3.
 
     return newSumText;
